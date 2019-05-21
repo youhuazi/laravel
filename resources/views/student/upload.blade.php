3a2aa3fb77bf51bf84a4eae51fa203db98b7c93a
yonghua you <yonghua-you@dac.co.jp>
2019-05-21 11:35:26 +0900

    test

--- resources/views/student/upload.blade.php
+++ resources/views/student/upload.blade.php
@@ -24,7 +24,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    confirm upload
+                                    confirm upload !
                                 </button>
 
                             </div>
